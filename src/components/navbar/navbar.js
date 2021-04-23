8597d19a69b47d174414b8536d7bab208c515da1
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-23 14:11:28 +0100

    correction

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,5 @@
 import './navbar.css'
+import {FiTruck, IoCallOutline} from "react-icons/all";
 
 const Navbar = () => {
     return (
@@ -7,9 +8,11 @@ const Navbar = () => {
             <div className={'navbar-content'}>
                 <div className={'navbar-items-left'}>
                     <div className={'navbar-first'}>
+                        <IoCallOutline/>
                         <span>Appelez-nous au : 01 30 95 56 10.</span>
                     </div>
                     <div className={'navbar-secend'}>
+                        <FiTruck/>
                         <span>Livraison offerte en point de retrait dès 59€ *</span>
                     </div>
                 </div>
