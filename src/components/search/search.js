9c4cca50d8b73665e3f41bad2a074f53d18be5fb
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-23 15:17:50 +0100

    correction

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -3,7 +3,18 @@ import './search.css'
 const Search = () => {
     return (
         <div className={'search-main'}>
-
+            <div className={'search-left'}></div>
+            <div className={'search-content'}>
+                <div className={'sr-logo'}>
+                    <img
+                        src={'https://www.medicament.com/img/medicamentcom-vente-en-ligne-de-m%C3%A9dicaments-logo-1502891216.jpg'}/>
+                </div>
+                <div className={'sr-input'}>
+                    <input placeholder={'Chercher'}/>
+                </div>
+                <div className={'sr-panier'}></div>
+            </div>
+            <div className={'search-right'}></div>
         </div>
     )
 }
