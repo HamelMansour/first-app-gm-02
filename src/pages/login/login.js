cfdbb31cf4226157b11e8d163cfd1fdf995bf007
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-23 10:58:20 +0100

    correction

---
+++ src/pages/login/login.js
@@ -0,0 +1,31 @@
+import './login.css'
+import loginImg from '../../assets/images/paypal-logo-129x32.svg'
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="logo">
+                    <img src={loginImg} alt="Logo paypal"/>
+                </div>
+                <div className="inputs">
+                    <input type="text" placeholder="Email ou némuro de mobile"/>
+                    <input type="password" placeholder="Mot de passe"/>
+                    <div classNameName="btn next">
+                        <span>Suivant</span>
+                    </div>
+                    <div className="divider">
+                        <div className="left"></div>
+                        <span>OU</span>
+                        <div className="right"></div>
+                    </div>
+                    <div className="btn register-btn">
+                        <span>Ouvrir un compte</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
