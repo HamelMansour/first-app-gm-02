25e8e12dce70a74076a4ab1e2be695f32de85db0
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-23 15:54:41 +0100

    correction

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,6 +1,12 @@
 import './search.css'
+import {AiFillCaretDown, FaShoppingBasket} from "react-icons/all";
 
 const Search = () => {
+    let retation = 'rotate(0)';
+    const chageRetation = () => {
+        retation = 'rotate(20deg)'
+        document.getElementById('icon').style.transform = retation
+    }
     return (
         <div className={'search-main'}>
             <div className={'search-left'}></div>
@@ -12,7 +18,13 @@ const Search = () => {
                 <div className={'sr-input'}>
                     <input placeholder={'Chercher'}/>
                 </div>
-                <div className={'sr-panier'}></div>
+                <div className={'sr-panier'}>
+                    <div className={'sr-panier-content'}>
+                        <FaShoppingBasket/>
+                        <span>Panier</span>
+                        <AiFillCaretDown/>
+                    </div>
+                </div>
             </div>
             <div className={'search-right'}></div>
         </div>
